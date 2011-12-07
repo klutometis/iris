1e72b09e4125182521c7c4b43839d8bdf28d98b4
Peter Danenberg <pcd@roxygen.org>
2011-12-07 11:59:20 -0800

    `lein deps' gives me a mechanism to populate lib, even for literate programming.

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject iris "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [funnyplaces "1.1-beta"]
+                 [cadr "1.0.1-SNAPSHOT"]
+                 [lambda "1.0.1-SNAPSHOT"]
+                 [debug "1.0.0-SNAPSHOT"]
+                 [clj-http "0.1.3"]
+                 [org.clojure/data.json "0.1.1"]
+                 [net.sourceforge.javaflacencoder/java-flac-encoder "0.2.3"]]
+  :repositories {"conjars" "http://conjars.org/repo/"})
