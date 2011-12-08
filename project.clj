ed8e9a71f10e207427f3e844d03410f44f4a4b13
Peter Danenberg <pcd@roxygen.org>
2011-12-07 22:53:59 -0800

    Answering a `find' query; TODO: general fallback-query.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [cadr "1.0.1-SNAPSHOT"]
                  [lambda "1.0.1-SNAPSHOT"]
                  [debug "1.0.0-SNAPSHOT"]
-                 [clj-http "0.1.3"]
+                 [clj-http "0.2.5"]
                  [org.clojure/data.json "0.1.1"]
                  [net.sourceforge.javaflacencoder/java-flac-encoder "0.2.3"]
                  [de.huxhorn.sulky/de.huxhorn.sulky.3rdparty.jlayer "1.0"]]
