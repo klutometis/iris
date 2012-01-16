29c8ebde47b8fb282deb918cef010c268ab4ec5b
Peter Danenberg <pcd@roxygen.org>
2012-01-15 19:56:09 -0800

    Almost generating what we need; missing a recursive step.

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [funnyplaces "1.2-beta"]
                  [cadr "1.0.1-SNAPSHOT"]
-                 [lambda "1.0.1-SNAPSHOT"]
+                 [lambda "1.0.2-SNAPSHOT"]
                  [clj-http "0.2.5"]
                  [org.clojure/data.json "0.1.1"]
                  [net.sourceforge.javaflacencoder/java-flac-encoder "0.2.3"]
                  [de.huxhorn.sulky/de.huxhorn.sulky.3rdparty.jlayer "1.0"]]
-  :dev-dependencies [[debug "1.0.0-SNAPSHOT"]
-                     [add-classpath "1.0.2-SNAPSHOT"]]
+  :dev-dependencies [[debug "1.0.0-SNAPSHOT"]]
   :repositories {"conjars" "http://conjars.org/repo/"}
   :main iris.core)
