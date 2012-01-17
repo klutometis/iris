9e76652bb9614622be6cf5827353149db693396f
Peter Danenberg <pcd@roxygen.org>
2012-01-17 14:08:07 -0800

    Finally constructing some queries

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [clj-http "0.2.5"]
                  [org.clojure/data.json "0.1.1"]
                  [net.sourceforge.javaflacencoder/java-flac-encoder "0.2.3"]
-                 [de.huxhorn.sulky/de.huxhorn.sulky.3rdparty.jlayer "1.0"]]
+                 [de.huxhorn.sulky/de.huxhorn.sulky.3rdparty.jlayer "1.0"]
+                 [slingshot "0.10.1"]]
   :dev-dependencies [[debug "1.0.0-SNAPSHOT"]]
   :repositories {"conjars" "http://conjars.org/repo/"}
   :main iris.core)
